1991dfd696c8aa9d5dd7ba63c63beaaedb0b91b4
marcindev10 <marcinselecki@gmail.com>
2019-08-21 14:54:01 +0300

    adding new files

---
+++ script.js
@@ -0,0 +1,81 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right,  " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+ 
+
+
+let person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+let firstName = person.firstName;
+let lastName = person.lastName;
+let age = person.age;
+let eyeColor = person.eyeColor;
+
+
+let a = 'test';
+let b = true;
+let c = 789;
+
+let okObj = {
+  a: a,
+  b: b,
+  c: c
+};
+
+
+
+
+const addTo = x => y => x + y
+var addToTen = addTo(10)
+addToTen(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
